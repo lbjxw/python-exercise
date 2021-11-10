7500730fa9c0e2da2e71e41a5118d93195fba259
lbjxw <1336748051@qq.com>
2021-11-10 22:58:27 +0800

    seventh commit

---
+++ test10'while'循环.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+sum = 0
+n = 100
+while n > 0:
+    sum += n
+    n = n - 2
+print(sum)
+
+
+a = 1
+sum = 0
+while a <= 100:
+    if a%2 == 0:
+        sum+=a
+    a = a + 1
+print('1-100之间的偶数和',sum)
+
+    
+
+    
